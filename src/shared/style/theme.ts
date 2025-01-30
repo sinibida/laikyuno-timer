432b02601c17091b81d16870bfae7d14e53c016d
SPAUPA <40449398+sinibida@users.noreply.github.com>
2025-01-30 15:53:48 +0900

    initial

---
+++ src/shared/style/theme.ts
@@ -0,0 +1,28 @@
+"use client";
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "var(--font-pretendard)",
+    h1: {
+      fontWeight: 700,
+    },
+    h2: {
+      fontWeight: 700,
+    },
+    h3: {
+      fontWeight: 700,
+    },
+    h4: {
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 700,
+    },
+    h6: {
+      fontWeight: 700,
+    },
+  },
+});
+
+export default theme;
