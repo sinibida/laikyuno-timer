86d04761a82a918fa1a8cdcad6f518780e35af0c
SPAUPA <40449398+sinibida@users.noreply.github.com>
2025-02-09 17:23:48 +0900

    +: regexes revamp

--- src/domains/Parser/classes/Parser/lib/regexes.ts
+++ src/domains/Parser/classes/Parser/lib/regexes.ts
@@ -1,22 +1,22 @@
 import { TimerRegexData } from "../../TimerRegex";
 
+// https://stackoverflow.com/a/59488150
+const concat = (...datas: TimerRegexData[]): TimerRegexData => ({
+  pattern: new RegExp(datas.map((data) => data.pattern.source).join("")),
+  groupKeys: datas.map((x) => x.groupKeys).flat(),
+});
+
+const timeRegex: TimerRegexData = {
+  pattern: /(?:(\d+)h)?(?:(\d+)m)?(?:(\d+)s?)?/,
+  groupKeys: ["hours", "minutes", "seconds"],
+};
+
 const regexes: TimerRegexData[] = [
-  {
-    pattern: /(\d+)m(\d+)s?x(\d+)/,
-    groupKeys: ["minutes", "seconds", "times"],
-  },
-  {
-    pattern: /(\d+)x(\d+)/,
-    groupKeys: ["seconds", "times"],
-  },
-  {
-    pattern: /(\d+)m(\d+)s?/,
-    groupKeys: ["minutes", "seconds"],
-  },
-  {
-    pattern: /(\d+)/,
-    groupKeys: ["seconds"],
-  },
+  concat(timeRegex, {
+    pattern: /x(\d+)/,
+    groupKeys: ["times"],
+  }),
+  timeRegex,
 ];
 
 export default regexes;
